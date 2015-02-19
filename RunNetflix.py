96404296b93c3d4d917933afb29100e128eddd28
LHBDev <freeraprb@gmail.com>
2015-02-19 00:57:37 -0600

    Initial commit. Fixed #1. Fixed #2. Fixed #6.

---
+++ RunNetflix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# --------
+# imports
+# --------
+
+from io 	  import StringIO
+from unittest import main, TestCase
+import Netflix
+
+
+# ------------
+# TestNetflix
+# ------------
+
+class TestNetflix (TestCase):
